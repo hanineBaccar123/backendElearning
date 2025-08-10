0647bb0297c8251a1f0efc5fa809f034cdf0672b
hanineBaccar123 <hanine.baccar@icloud.com>
2025-08-10 11:40:57 +0100

    go

--- myapp/app.js
+++ myapp/app.js
@@ -4,11 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const http = require("http");
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
 
 
 app.use(logger('dev'));
@@ -36,4 +37,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const server = http.createServer(app)
+server.listen(5000,()=>{console.log("app is running on port 5000")})
 module.exports = app;
