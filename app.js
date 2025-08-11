429bb3ff1d5db2464bfc799b977b2ba540e902d8
hanineBaccar123 <hanine.baccar@icloud.com>
2025-08-11 19:37:34 +0100

    go

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const http = require("http");
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -37,6 +38,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const server = http.createServer(app)
-server.listen(5000,()=>{console.log("app is running on port 5000")})
 module.exports = app;
