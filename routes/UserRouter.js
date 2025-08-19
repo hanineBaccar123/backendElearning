198c95575874f9ab275bda9529d812f61630f086
hanineBaccar123 <hanine.baccar@icloud.com>
2025-08-19 22:08:20 +0100

    user image

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -11,8 +11,8 @@ router.get('/getUserById/:id', UserController.getUsersById);
 router.post('/addClient', UserController.addClient);
 router.get('/searchUsersByFirstName', UserController.searchUsersByFirstName);
 router.delete('/deleteUserById/:id', UserController.deleteUserById);
-router.post('/addClientV2', uploadfile.single("user_image"),UserController.addClientV2);
-router.post('/addClientWithImage', UserController.addClientWithImage);
+router.post('/addClientV2',UserController.addClientV2);
+router.post('/addClientWithImage', uploadfile.single("user_image"),UserController.addClientWithImage);
 
 
 
